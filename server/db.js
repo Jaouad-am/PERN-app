0c4b339aa88f9f8fcf21fe5f763de805d24c50f5
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-08-13 23:12:46 +0100

    Connect Postgres DB with Server

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "root",
+  host: "localhost",
+  port: 5432,
+  database: "pernapp",
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const pool = require("./db");
 
 //middleware
 app.use(cors());
 //access to req.body data
 app.use(express.json());
+
+//routes
+
+//create a todo
+
+//get todos list
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
