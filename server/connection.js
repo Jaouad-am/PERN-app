598679b18404e74ec7b9a9f3841edfab2d227a55
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-08-21 02:08:53 +0100

    adding INSERT query & get all todos query

--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
 const pool = new Pool({
+  host: "localhost",
   user: "postgres",
   password: "root",
-  host: "localhost",
-  port: 5432,
   database: "pernapp",
+  port: 5432,
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,25 +1,51 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const pool = require("./db");
+const cors = require("cors");
+const pool = require("./connection");
+
+//starting the server on port 5000
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
 
 //middleware
 app.use(cors());
 //access to req.body data
-app.use(express.json());
+app.use(express.json()); //req.body
 
-//routes
+/******routes******/
 
 //create a todo
 
-//get todos list
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const list = await pool.query(
+      "insert into todo (description) values($1) returning *", // returning the row that was inserted
+      [description]
+    );
+    //wait for the query to be done
+    //let result = await list;
+    //gets the row inserted (using returning *)
+    res.json(list.rows[0]);
+    //res.send("todo added!!");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
+//get todos list
+app.get("/todos", async (req, res) => {
+  try {
+    const list = await pool.query("select * from todo");
+    res.json(list.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 //get a todo
 
 //update a todo
 
 //delete a todo
-
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
-});
