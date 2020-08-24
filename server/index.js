4b40d5702ca83e51a6025d78a787b849171ceae1
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-08-24 23:01:24 +0100

    Update a todo query

--- server/index.js
+++ server/index.js
@@ -55,5 +55,19 @@ app.get("/todos/:id", async (req, res) => {
   }
 });
 //update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //delete a todo
