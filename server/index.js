c8db9d5cc8cedfdd522006d5e102854192889972
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-08-25 20:38:27 +0100

    Delete a todo Query

--- server/index.js
+++ server/index.js
@@ -71,3 +71,15 @@ app.put("/todos/:id", async (req, res) => {
 });
 
 //delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
