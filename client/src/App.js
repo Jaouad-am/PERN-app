89f6295973a3e45aa38222c59694318034cf00da
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-08-26 22:13:40 +0100

    setting up the Client side with React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>hello from app.js</h1>
+    </Fragment>
+  );
+}
+
+export default App;
