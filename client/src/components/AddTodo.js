204e2d005885b2ac2c858020af86afebcbb15651
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-08-28 02:05:42 +0100

    Finishing AddTodo component- useState/onSubmit/onChange

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,11 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const AddTodo = () => {
+  const [description, setDescription] = useState("");
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      //console.log(response);
+      window.location("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Fragment>
       <h1 className="text-center mt-5">add todo Component</h1>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" />
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button className="btn btn-success">Add</button>
       </form>
     </Fragment>
