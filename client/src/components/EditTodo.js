5a2cf22a7e0578ed1a1a4d59b6459c9532730d8d
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-09-07 19:57:09 +0100

    EditDescription function

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -3,23 +3,55 @@ import React, { Fragment, useState } from "react";
 const EditTodo = ({ todo }) => {
   const [description, setDescription] = useState(todo.description);
   console.log(todo);
+
+  //EditDescription function
+
+  const EditDescription = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      //console.log(response);
+      window.location("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Fragment>
       <button
         type="button"
         class="btn btn-warning"
         data-toggle="modal"
-        data-target={`id${todo.todo.id}`}
+        data-target={`id${todo.todo_id}`}
       >
         Edit
       </button>
       {/* example id = id10 */}
-      <div class="modal" id={`id${todo.todo.id}`}>
+      <div
+        class="modal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
+        {/* setDescription back to original if we cliack away from the modal */}
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
               <h4 class="modal-title">Modal Heading</h4>
-              <button type="button" class="close" data-dismiss="modal">
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                {/* setDescription back to original if we close the modal */}
                 &times;
               </button>
             </div>
@@ -38,10 +70,17 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 class="btn btn-warning"
                 data-dismiss="modal"
+                onclick={(e) => EditDescription(e)}
               >
                 Edit
               </button>
-              <button type="button" class="btn btn-danger" data-dismiss="modal">
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                {/* setDescription back to original if we close edit description */}
                 Close
               </button>
             </div>
